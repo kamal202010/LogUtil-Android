15bbd03c0d1a4092ac0ad9bf7bd5d8b90ed98b75
Kamal <kamal.khalaf@businessboomers.net>
2019-10-16 13:49:47 +0200

    Initial commit

---
+++ myfirstlibr/src/main/java/com/khalaf/kamal/myfirstlibr/LogDebug.java
@@ -0,0 +1,18 @@
+package com.khalaf.kamal.myfirstlibr;
+/*
+ *
+ * Created by Kamal Khalaf on 10/16/2019.
+ * Contact : kamal.khalaf56@gmail.com
+ *
+ */
+
+import android.util.Log;
+
+public class LogDebug {
+
+    public static final String Tag = "SUPER_AWSOM_APP";
+
+    public static void d(String message){
+        Log.d(Tag , message);
+    }
+}
